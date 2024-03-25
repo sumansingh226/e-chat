39009cf4811ba4ef1a82c78e529841ae2f08864a
sumansingh226 <sumansingh13062000@gmail.com>
2024-03-25 18:04:20 +0530

    signup

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
+import { ThemeProvider } from "@mui/material";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./screens/HomePage/Home";
+import { createTheme } from "@mui/material/styles";
 
-// Define your URLs
-const urls = {
-  landingViewPath: "/",
-  loginViewPath: "/login",
-  registerViewPath: "/register",
-  forgotPasswordViewPath: "/forgot-password",
-  changePasswordViewPath: "/change-password",
-};
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      "MyriadPro_Light",
+      "MyriadPro_Bold",
+      "MyriadPro_Medium",
+      "MyriadPro_Regular",
+      "sans-serif",
+      "Digital-7 Mono",
+    ].join(","),
+  },
+});
 
 const App = () => {
   return (
-    <div>
+    <ThemeProvider theme={{}}>
       <Router>
         <Routes>
           <Route path={urls.landingViewPath} element={<Home />} />
@@ -22,7 +30,7 @@ const App = () => {
           <Route path={urls.forgotPasswordViewPath} element={<Home />} />
         </Routes>
       </Router>
-    </div>
+    </ThemeProvider>
   );
 };
 
--- src/global/constants/UrlConstants.ts
+++ src/global/constants/UrlConstants.ts
@@ -1,13 +1,13 @@
-
-class urlConstants extends String {
+class UrlConstants {
     live = false;
     landingViewPath = "/";
     loginViewPath = "/login";
+    registerViewPath = "/register";
     forgotPasswordViewPath = "/forgot-password";
-    changePasswordViewPath = "/changepwd";
+    changePasswordViewPath = "/change-password";
     signup = "/signup";
 }
 
-const urls = new urlConstants();
+const urls = new UrlConstants();
 
 export default urls;
