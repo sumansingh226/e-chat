db7e606366a31de19e3b798752029cd57f99d470
sumansingh226 <sumansingh13062000@gmail.com>
2024-03-25 17:58:10 +0530

    signup

--- src/global/constants/UrlConstants.ts
+++ src/global/constants/UrlConstants.ts
@@ -1,10 +1,11 @@
 
 class urlConstants extends String {
-
+    live = false;
     landingViewPath = "/";
     loginViewPath = "/login";
     forgotPasswordViewPath = "/forgot-password";
     changePasswordViewPath = "/changepwd";
+    signup = "/signup";
 }
 
 const urls = new urlConstants();
