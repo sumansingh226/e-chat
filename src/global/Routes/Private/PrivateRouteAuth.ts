cd5adac437fb6c8692ee46f7f79c6fa3108d370d
sumansingh226 <sumansingh13062000@gmail.com>
2024-04-19 00:32:12 +0530

    define moment helpers  and string constants

--- src/global/Routes/Private/PrivateRouteAuth.ts
+++ src/global/Routes/Private/PrivateRouteAuth.ts
@@ -1,16 +1,16 @@
 import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
-import { useAuth } from "../Context/useAuth";
+// import { useAuth } from "../Context/useAuth";
 
 type Props = { children: React.ReactNode };
 
 const ProtectedRoute = ({ children }: Props) => {
-    const location = useLocation();
-    const { isLoggedIn } = useAuth();
-    return isLoggedIn() ? (
-        <>{ children } < />
-    ) : (
-        <Navigate to= "/login" state = {{ from: location }
+  const location = useLocation();
+  const { isLoggedIn } = useAuth();
+  return isLoggedIn() ? (
+    <>{ children } < />
+  ) : (
+    <Navigate to= "/login" state = {{ from: location }
 } replace />
   );
 };
