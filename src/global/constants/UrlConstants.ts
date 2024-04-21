0694900f207c536824ecca46309c1e70f52e7549
sumansingh226 <sumansingh13062000@gmail.com>
2024-04-21 23:24:48 +0530

    auth private route

--- src/global/constants/UrlConstants.ts
+++ src/global/constants/UrlConstants.ts
@@ -1,11 +1,22 @@
 class UrlConstants {
+    // Set this to true for live environment, false for local environment
     live = false;
+
+    apiUrl = this.live ? "https://live.api.com" : "http://localhost:5000";
+
+    // Paths for different views
     landingViewPath = "/";
     loginViewPath = "/login";
     registerViewPath = "/register";
     forgotPasswordViewPath = "/forgot-password";
     changePasswordViewPath = "/change-password";
     signup = "/signup";
+
+    // Additional URLs for echat
+    echatBasePath = "/echat";
+    chatRoomsPath = this.echatBasePath + "/rooms";
+    userProfilePath = this.echatBasePath + "/profile";
+    // Add more URLs as needed
 }
 
 const urls = new UrlConstants();
